2aebcb14f23dee86cf3679a5991d8e9915eb7703
BiwBong1706 <bewrockgame1@gmail.com>
2026-08-08 15:35:55 +0700

    Update axios.js

--- expense-frontend/src/api/axios.js
+++ expense-frontend/src/api/axios.js
@@ -1,31 +1,47 @@
-//axios.js
+// src/api/axios.js
 
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-  },
+    baseURL: import.meta.env.VITE_API_URL,
+    withCredentials: true,
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
 
 api.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  (error) => {
+    (response) => {
+        return response;
+    },
 
-    if (error.response?.status === 401) {
+    (error) => {
 
-      localStorage.removeItem("token");
+        if (error.response?.status === 401) {
 
-      window.location.href = "/";
+            localStorage.removeItem("token");
+
+            // URL ของ API ที่ทำให้ 401
+            const url = error.config?.url || "";
+
+            // ถ้าเป็น /auth/profile
+            // ห้าม redirect เพราะ PublicRoute
+            // ใช้ 401 เพื่อเช็กว่ายัง Login หรือไม่
+            if (!url.includes("/auth/profile")) {
+
+                if (window.location.pathname !== "/") {
+
+                    window.location.href = "/";
+
+                }
+
+            }
+
+        }
+
+        return Promise.reject(error);
 
     }
-
-    return Promise.reject(error);
-  }
 );
 
 export default api;
