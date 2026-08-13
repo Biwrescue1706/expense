1fe0efbc541f144769aa3002847d7f0e6fd2e445
Biwbong <bewrockgame1@gmail.com>
2026-08-13 18:41:53 +0700

    1

--- expense-frontend/vite.config.js
+++ expense-frontend/vite.config.js
@@ -1,10 +1,40 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "ระบบบันทึกรายรับรายจ่าย",
+        short_name: "รายรับรายจ่าย",
+        description: "ระบบบันทึกรายรับรายจ่าย",
+        start_url: "/",
+        scope: "/",
+        display: "standalone",
+
+        theme_color: "#4CAF50",
+        background_color: "#ffffff",
+
+        icons: [
+          {
+            src: "/BiwBoong.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/BiwBoong.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
   ],
-})
+});
