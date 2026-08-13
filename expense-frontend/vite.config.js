7375f8fe8bb1b059dc00b3b387e4ae6839f3137b
Biwbong <bewrockgame1@gmail.com>
2026-08-13 19:42:02 +0700

    Update vite.config.js

--- expense-frontend/vite.config.js
+++ expense-frontend/vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
 
     VitePWA({
       registerType: "autoUpdate",
@@ -13,6 +15,7 @@ export default defineConfig({
         name: "ระบบบันทึกรายรับรายจ่าย",
         short_name: "รายรับรายจ่าย",
         description: "ระบบบันทึกรายรับรายจ่าย",
+
         lang: "th",
 
         start_url: "/",
